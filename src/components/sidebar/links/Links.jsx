3cd2a5b092a8f8c2d2c3b407c15489767a6490ff
Chammi Hansana <84610582+chammi-hansana@users.noreply.github.com>
2023-11-29 23:02:15 +0530

    new features added commit

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -3,16 +3,31 @@ import {motion} from "framer-motion";
 const variants = {
     open:{
       transition:{
-        staggeredChildren : 0.1,
+        staggerChildren : 0.1,
       },
     },
 
     closed:{
       transition:{
-        staggeredChildren : 0.05,
-        staggeredDirection : -1,
+        staggerChildren : 0.05,
+        staggerDirection : -1,
       },
     },
+};
+
+const itemVariants = {
+  open:{
+   y:0,
+   opacity:1,
+
+  },
+
+  closed:{
+    transition:{
+      y:50,
+      opacity:0,
+    },
+  },
 };
 const Links = () => {
   const items = ["Homepage","Services","Portfolio","Contact", "About"];
@@ -21,9 +36,10 @@ const Links = () => {
     return (
       <motion.div className="links" variants={variants}> 
       {items.map((item) => (
-        <a href = {`#${item}`} key={item}>
+        <motion.a href = {`#${item}`} key={item} variants={itemVariants} whileHover={{scale:1.1}}
+        whileTap={{scale:0.95}}>
           {item}
-        </a>
+        </motion.a>
       ))} 
         
       </motion.div>
