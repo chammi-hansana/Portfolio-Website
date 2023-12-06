7c28ce348e2a71b9a29bfe0e1a943ca98074ad3c
chammi-hansana <chammi2819@gmail.com>
2023-12-06 12:48:31 +0530

    test

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,80 +1,98 @@
-import "./services.scss"
-import {motion} from "framer-motion";
+import "./services.scss";
+import { motion } from "framer-motion";
 
-const variants= {
-    initial:{
-        x:-500,
-        y:100,
-        opacity:0,
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
     },
-    animate:{
-        x:0,
-        opacity:1,
-        y:0,
-        transition:{
-            duration:1,
-            staggerChildren:0.1,
-        },
-    },
+  },
 };
 const Services = () => {
   return (
-    <motion.div className="services" variants={variants} initial="initial" animate="animate">
-        <motion.div className="textContainer" variants={variants} >
-            <p>
-                I focus on helping your brand grow <br/>and move forward
-            </p>
-            <hr/>
-        </motion.div>
-        <motion.div className="titleContainer" variants={variants} >
-            <div className="title">
-                <img src="/people.webp" alt="" />
-                <h1><b>Unique</b> Ideas</h1>
-            </div>
-        
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      animate="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand grow <br />
+          and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <b>Unique</b> Ideas
+          </h1>
+        </div>
 
-      
-            <div className="title">
-             
-                <h1><b>For Your</b> Business.</h1>
-                <button>WHAT WE DO?</button>
-            </div>
+        <div className="title">
+          <h1>
+            <b>For Your</b> Business.
+          </h1>
+          <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
+          </p>
+          <button>Go</button>
         </motion.div>
-        <motion.div className="listContainer" variants={variants} >
-          <motion.div className="box" whileHover={{background:"lightgray" , color:"black"}}>
-            <h2>Branding</h2>
-            <p>
-                loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
-            </p>
-            <button>Go</button>
-          </motion.div>
 
-          <motion.div className="box" whileHover={{background:"lightgray" , color:"black"}}>
-            <h2>Branding</h2>
-            <p>
-                loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
-            </p>
-            <button>Go</button>
-          </motion.div>
-
-          <motion.div className="box" whileHover={{background:"lightgray" , color:"black"}}>
-            <h2>Branding</h2>
-            <p>
-                loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
-            </p>
-            <button>Go</button>
-          </motion.div>
-          <motion.div className="box" whileHover={{background:"lightgray" , color:"black"}}>
-            <h2>Branding</h2>
-            <p>
-                loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
-            </p>
-            <button>Go</button>
-          </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
+          </p>
+          <button>Go</button>
         </motion.div>
-      
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            loremfsdfsjdfsdfhsdjfsdnfcdsjfhkdjsddljshfjdsflsdflkadfdsfsjdkcxjcsf
+          </p>
+          <button>Go</button>
+        </motion.div>
+      </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
 export default Services;
